7b596402c91137af7b13122a032ae00d40a1aefe
Roshan Ali <roshanalink321@gmail.com>
2025-08-12 21:01:52 +0530

    12th aug 9pm

--- src/header/ThreeDotMore.jsx
+++ src/header/ThreeDotMore.jsx
@@ -55,7 +55,7 @@ function ThreeDotMore({onClose}) {
                             <img src={download} alt="" />
                         </div>
                         Download App
-                    </li>
+                    </li>  
                 </a>
             </ul>
 
