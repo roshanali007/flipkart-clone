7ffed9a630e7fdc7f00c2520c1d54d0748ff7ade
Roshan Ali <roshanalink321@gmail.com>
2025-09-01 20:21:32 +0530

    1th sep 1pm

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/flipkart-clone/',
+  base:process.env.FLIPKART_CLONE || "/flipkart-clone",
 })
