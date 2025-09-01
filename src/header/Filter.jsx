46ba1a689818a2a204774157199fc883c8e09c24
Roshan Ali <roshanalink321@gmail.com>
2025-09-01 08:53:53 +0530

    30th aug 12.00pm

--- src/header/Filter.jsx
+++ src/header/Filter.jsx
@@ -1,26 +1,23 @@
 import React from 'react'
+import goback_arrow from '../assets/goback_arrow_black.svg'
+import './filter.css'
+import {filter} from '../data/filter.json'
 
 function Filter() {
   return (
     <div className='filter_main'>
-        <div className='filter_header'></div>
+        <div className='filter_header'>
+          <div className='filter_goback'>
+              <img src={goback_arrow} alt="" className='arrow_back' />
+          </div>
+          <span className='filter_text'>Filters</span>
+        </div>
         <div className='filter_content'>
-            <div className='filter_each'><span>Price</span></div>
-            <div className='filter_each'><span>F-Assured</span></div>
-            <div className='filter_each'><span>Brand</span></div>
-            <div className='filter_each'><span>Customer Ratings</span></div>
-            <div className='filter_each'><span>Offers</span></div>
-            <div className='filter_each'><span>Discount</span></div>
-            <div className='filter_each'><span>Gst Invoice Available</span></div>
-            <div className='filter_each'><span>Availability</span></div>
-            <div className='filter_each'><span>container type</span></div>
-            <div className='filter_each'><span>New Arrivals</span></div>
-            <div className='filter_each'><span>Value Packs</span></div>
-            <div className='filter_each'><span>Organic</span></div>
-            <div className='filter_each'><span>Type</span></div>
-            <div className='filter_each'><span>festival_marker</span></div>
-            <div className='filter_each'><span>Pack of</span></div>
-            <div className='filter_each'><span>Category</span></div>
+          {
+              filter.map((item)=>{    
+                return <div className='filter_each'><span>{item.name}</span></div>
+              })
+          }
         </div>      
     </div>
   )
