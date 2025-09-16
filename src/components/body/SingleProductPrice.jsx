5b5aa02da4ac94ddb535b16fb34550da00849eb9
Roshan Ali <roshanalink321@gmail.com>
2025-09-16 08:41:02 +0530

    15th sep 7.30pm

---
+++ src/components/body/SingleProductPrice.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './SingleProductPrice.css'
+import white_star from '/images/white_star.svg'
+import assurance from '/images/assurance.png'
+import price_info from '/images/price_info.svg'
+
+function SingleProductPrice() {
+  return (
+    <div className='product_details_price'>
+        <div>
+            <h1 className='product_name'>
+                Happilo Premium Naturals Californian Almonds Almonds  (1 x 200 g)
+            </h1>
+        </div>
+        <div className='product_details_rating'>
+            <div className='product_details_overall'>
+                <div className='product_overall_rating'>
+                    4.4
+                    <img src={white_star} alt=""  className='white_star'/>
+                </div>
+                <span className='product_customer_rating'>
+                    <span className='product_customer_number'>1,95,240 Ratings & 17,160 Reviews</span>
+                </span>
+            </div>
+            <span className='product_fassured'>
+                <img src={assurance} alt="" className='product_assure_img' />
+            </span>
+        </div>
+        <div className='product_special_price' >
+            <span className='special_price_text'>Special Price</span>
+        </div>
+        <div className='product_price'>
+            <div className='product_discount'>
+                <div className='price'>₹224</div>
+                <div className='overline'>₹275</div>
+                <div className='discount'>18% off</div>
+            </div>
+            <div className='product_price_info'>
+                <img src={price_info} alt="" className='price_info_img' />
+            </div>
+            <div className='product_quantity_info'>@280/250g</div>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default SingleProductPrice
--- src/components/products/ProductDetails.jsx
+++ src/components/products/ProductDetails.jsx
@@ -1,32 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import cart_img from '/images/cart_img.svg'
+import './ProductDetails.css'
+import {imageList,imageSelect,path} from '../adData/ProductDetails.json'
+import share_img from '/images/share_img.svg'
+import SingleProductPrice from '../body/SingleProductPrice'
+
 
 function ProductDetails() {
-     const imageList=[
-        {
-            id:1,
-            src:"/images/imageList_1happilo.jpeg"
-        },
-        {
-            id:2,
-            src:"/images/imageList_2happilo.webp"
-        },
-        {
-            id:3,
-            src:"/images/imageList_3happilo.webp"
-        },
-        {
-            id:3,
-            src:"/images/imageList_4happilo.jpeg"
-        },
-        {
-            id:3,
-            src:"/images/imageList_5happilo.webp"
-        },
-        {
-            id:3,
-            src:"/images/imageList_6happilo.jpeg"
-        }
-     ]
+    const [selectedId,setSelectedId] =useState(1)
+    const handleClick = (item)=>{
+        setSelectedId(item)
+    }
+     const selectedImage = imageSelect.find(img=>img.id === selectedId)
+
   return (
     <div className='product_details_main'>
         <div className='product_details_div'>
@@ -36,10 +22,10 @@ function ProductDetails() {
                         <ul className='product_img_ul'>
                             {
                                 imageList.map((item)=>{
-                                    return <li className='product_img_li'>
+                                    return <li className={`${item.id===6?'product_img_li_last':'product_img_li'}`} onClick={()=>handleClick(item.id)} key={item.id}>
                                                 <div className='product_img_main'>
-                                                    <div className='product_img_div'>
-                                                        <img src={item.src} alt="" className='product_img' />
+                                                    <div className='product_img_div_list'>
+                                                        <img src={item.src} alt="" className='product_img_list' />
                                                     </div>
                                                 </div>
                                             </li>
@@ -49,16 +35,70 @@ function ProductDetails() {
                     </div>
                     <div className='product_selected_img'>
                         <div className='img_render_div'>
-                            <img src="/images/imageList_1happilo.jpeg" alt="" className=''/>   
+                            {
+                                (
+                                    <img src={selectedImage.src} alt="" className='selected_img'/>
+                                )
+                            }  
                         </div>
                     </div>
+                </div>
+                <div className='product_cart_buy'>
+                    <div className='product_cart_buy_div'>
+                        <ul className='product_cart_buy_ul'>
+                            <li className='product_cart'>
+                                <button className='product_cart_btn'>
+                                    <img src={cart_img} alt="" className='product_cart_img' />
+                                    ADD TO CART
+                                </button>
+                            </li>
+                            <li className='product_buy'>
+                                <button className='product_buy_btn'>
+                                    <span className='buy_img'></span>
+                                    BUY NOW
+                                </button>
+                            </li>
+                        </ul>
+                    </div>   
+                </div>
+            </div>
+            <div className='product_details_right'>  
+                <div className='product_details_path' >
+                    <div className='product_details_pathWay'>
+                        {
+                            path.map((item)=>{
+                                return  <div key={item.id} className='product_details_eachPath'>
+                                            <span className='product_details_pathName'>{item.name}</span>
+                                            <img src={item.src} alt="" className={`${item.src===null?'':'product_details_pathImg'}`} />
+                                        </div>
+                            })
+                        }
+                    </div>
+                    <div className='product_details_share'>
+                        <div className='product_share_div'>
+                            <img src={share_img} alt=""  className='product_share_img'/>
+                            <span className='product_share_text'>Share</span>
+                        </div>
+                    </div>
+                </div> 
+                <div>
+                    <SingleProductPrice/>
                 </div>
                 <div>
 
                 </div>
-            </div>
-            <div className='product_details_right'>  
+                <div>
 
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
             </div>
         </div>
     </div>
